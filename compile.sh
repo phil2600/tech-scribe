c5155a65d2220ab165976b98f49e098604b4c91f
Plopi42 <github@xc9.fr>
2012-10-05 12:28:33 +0200

    Compile with a sexy function

--- compile.sh
+++ compile.sh
@@ -2,15 +2,27 @@
 
 PATH_OUT=out
 PATH_EXTRA=extra
-
 OUTPUT_USEFUL=out/Useful.html
 OUTPUT_RAW=out/Raw.html
 
-INPUT_USEFUL="$(find useful -type f | tr '\n' ' ')"
-INPUT_USEFUL="${INPUT_USEFUL%?}"
-pandoc -s -S --toc --highlight-style=tango -c $PATH_EXTRA/pandoc.css -A $PATH_EXTRA/footer.html $INPUT_USEFUL -o $OUTPUT_USEFUL
+
+#
+# compile style outputName inputs...
+#
+compileHtml()
+{
+    style=$1
+    shift
+    outputName=$1
+    shift
 
 
-INPUT_RAW="$(find raw useful -type f | tr '\n' ' ')"
-INPUT_RAW="${INPUT_RAW%?}"
-pandoc -s -S --toc --highlight-style=tango -c $PATH_EXTRA/pandoc.css -A $PATH_EXTRA/footer.html $INPUT_RAW -o $OUTPUT_RAW
+    INPUT=$(find "$@" -type f | tr '\n' ' ')
+    INPUT="${INPUT%?}"
+    pandoc -s -S --toc "--highlight-style=$style" -c "$PATH_EXTRA/pandoc.css" -A "$PATH_EXTRA/footer.html" $INPUT -o "$outputName"
+}
+
+
+compileHtml tango $OUTPUT_USEFUL useful
+compileHtml tango $OUTPUT_RAW useful raw
+
