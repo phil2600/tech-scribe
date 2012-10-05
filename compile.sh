c20258b650b0166498ffe2f04ef9476b2d7e9bcd
Plopi42 <github@xc9.fr>
2012-10-05 16:16:10 +0200

    Adding a latex compilation function

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 
 PATH_OUT=out
 PATH_EXTRA=extra
-OUTPUT_USEFUL=out/Useful.html
-OUTPUT_RAW=out/Raw.html
+OUTPUT_USEFUL=out/Useful
+OUTPUT_RAW=out/Raw
 
 
 #
@@ -19,10 +19,29 @@ compileHtml()
 
     INPUT=$(find "$@" -type f | tr '\n' ' ')
     INPUT="${INPUT%?}"
-    pandoc -s -S --toc "--highlight-style=$style" -c "$PATH_EXTRA/pandoc.css" -A "$PATH_EXTRA/footer.html" $INPUT -o "$outputName"
+    pandoc -s -S --toc "--highlight-style=$style" -c "$PATH_EXTRA/pandoc.css" -A "$PATH_EXTRA/footer.html" $INPUT -o "$outputName.html"
+}
+
+#
+# compile style outputName inputs...
+#
+compileLaTex()
+{
+    style=$1
+    shift
+    outputName=$1
+    shift
+
+
+    INPUT=$(find "$@" -type f | tr '\n' ' ')
+    INPUT="${INPUT%?}"
+    pandoc -s -S --toc "--highlight-style=$style" -c "$PATH_EXTRA/pandoc.css" $INPUT -o "$outputName.tex"
 }
 
 
 compileHtml tango $OUTPUT_USEFUL useful
 compileHtml tango $OUTPUT_RAW useful raw
+
+compileLaTex tango $OUTPUT_USEFUL useful
+#compileLaTex tango $OUTPUT_RAW useful raw
 
